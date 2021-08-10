d37dc6fd4f8d619c23ca78b31bf6e9cc7fa39cf6
GorSharoyan <gor.sharoyan95@gmail.com>
2021-08-10 22:41:27 +0400

    :green_heart:ci(SwitchRouter):created SwithcRouter,starting to create routes

---
+++ src/components/Routes/SwithcRouter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+function SwitchRouter () {
+    return (
+        <main>
+            <h1>Hi I'm here</h1>
+        </main>
+    )
+}
+
+export default SwitchRouter
