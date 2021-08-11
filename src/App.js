a500811d8ec042276128976d1f4284b85a4d6bd1
GorSharoyan <gor.sharoyan95@gmail.com>
2021-08-11 23:54:42 +0400

    :art:(Footer):created custom footer ,started wworking on its UI

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import SwitchRouter from "./components/Routes/SwithcRouter";
 //components
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SwitchRouter />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./Footer.css";
+
+/*Icons*/
+// import LocationOnIcon from "@material-ui/icons/LocationOn";
+// import PhoneIcon from "@material-ui/icons/Phone";
+// import MailIcon from "@material-ui/icons/Mail";
+// import FacebookIcon from "@material-ui/icons/Facebook";
+// import LinkedInIcon from "@material-ui/icons/LinkedIn";
+// import TwitterIcon from "@material-ui/icons/Twitter";
+// import GitHubIcon from "@material-ui/icons/GitHub";
+
+export default function Footer() {
+  return <div>Your custom footer</div>;
+}
