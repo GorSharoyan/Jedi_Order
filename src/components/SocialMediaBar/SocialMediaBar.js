045713d7e709521a406e3deed1cfc7c24df58c26
GorSharoyan <gor.sharoyan95@gmail.com>
2021-08-12 23:02:07 +0400

    :art:UI(SocialMediaBar):worked on SocialMediaBar cards UI

--- src/components/SocialMediaBar/SocialMediaBar.js
+++ src/components/SocialMediaBar/SocialMediaBar.js
@@ -1,17 +1,32 @@
-//UI
+import React from "react";
 
+//UI
+import { makeStyles } from "@material-ui/core/styles";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import GitHubIcon from "@material-ui/icons/GitHub";
 
+//ReactRouter
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "25px",
+  },
+  element: {
+    padding: "5px",
+  },
+});
+
 export default function SocialMediaBar() {
+  const classes = useStyles();
   return (
-    <div>
-      <FacebookIcon />
-      <LinkedInIcon />
-      <TwitterIcon />
-      <GitHubIcon />
+    <div className={classes.root}>
+      <FacebookIcon className={classes.element} />
+      <LinkedInIcon className={classes.element} />
+      <TwitterIcon className={classes.element} />
+      <GitHubIcon className={classes.element} />
     </div>
   );
 }
