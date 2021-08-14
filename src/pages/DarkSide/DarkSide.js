45200f58bedcdeb4a2193d953cb014288965d148
GorSharoyan <gor.sharoyan95@gmail.com>
2021-08-15 01:24:29 +0400

    :green_heart:CI(ForceUserForm): created a separate file for forceuser registration

--- src/pages/DarkSide/DarkSide.js
+++ src/pages/DarkSide/DarkSide.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ForceUserCard from "../../components/ForceUserCard/ForceUserCard";
+import ForceUserForm from "../../components/ForceUserForm/ForceUserForm";
 
 export default function DarkSide() {
   return (
     <div>
       Siths
       <ForceUserCard />
+      <ForceUserForm />
     </div>
   );
 }
