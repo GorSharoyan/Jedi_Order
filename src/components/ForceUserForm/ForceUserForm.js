23e9209424b7599c64ba33260da6113b2272db74
GorSharoyan <gor.sharoyan95@gmail.com>
2021-08-15 01:43:31 +0400

    :green_heart:CI(ForceUserForm):created basic form for user addition

--- src/components/ForceUserForm/ForceUserForm.js
+++ src/components/ForceUserForm/ForceUserForm.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { useFormik, Formik } from "formik";
+import { Link } from "react-router-dom";
+
+//UI
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 export default function ForceUserForm() {
-  return <div>Register the forceuser</div>;
+  return (
+    <Formik initialValues={{}} onSubmit={() => {}}>
+      {(props) => (
+        <form onSubmit={props.handleSubmit}>
+          <div>
+            <h4>Position:</h4>
+            <TextField
+              // label={country}
+              id="outlined-basic"
+              type="text"
+              variant="outlined"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.description}
+              name="position"
+            />
+            {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+          </div>
+
+          <div>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={props.handleSubmit}
+            >
+              <Link to="/">Submit</Link>
+            </Button>
+          </div>
+        </form>
+      )}
+    </Formik>
+  );
 }
