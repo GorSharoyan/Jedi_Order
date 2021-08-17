ab7ba7bce61877c19ff93293e2dff636f552a5fd
GorSharoyan <gor.sharoyan95@gmail.com>
2021-08-18 00:54:19 +0400

    :green_heart:CI(ImageUpload):contionued integrating image upload services to the component

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -1,17 +1,48 @@
 import React from "react";
+import { useState } from "react";
+
 //UI
+import { makeStyles } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
+//services
+import uploadJediImage from "../../services/FirebaseServices/jedi.service";
+import uploadSithImage from "../../services/FirebaseServices/sith.service";
+
+let useStyles = makeStyles({
+  root: {
+    border: "5px solid green",
+  },
+});
+
 export default function ImageUpload() {
+  const classes = useStyles();
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleImageInput = ({ target: { files } }) => {
+    let img = files[0];
+    setImage(img);
+  };
+  const handleImageUpload = () => {};
+
   return (
-    <Button
-      variant="contained"
-      color="default"
-      // className={classes.button}
-      startIcon={<CloudUploadIcon />}
-    >
-      Upload
-    </Button>
+    <div className={classes.root}>
+      <Button
+        variant="contained"
+        color="default"
+        // className={classes.button}
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload
+      </Button>
+      <input
+        accept="image/*"
+        id="contained-button-file"
+        type="file"
+        onChange={handleImageInput}
+      />
+    </div>
   );
 }
